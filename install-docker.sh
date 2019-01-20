33a6e6e96391d5a31816b50cab2f4337bbd61edf
Oleg Chulanov <oleg.chulanov@gmail.com>
2019-01-20 09:50:20 +0200

    2

--- install-docker.sh
+++ install-docker.sh
@@ -1,14 +1,31 @@
-#!/bin/bash
+apt-get update
+    apt-get install -y apt-transport-https ca-certificates curl gnupg software-properties-common
+    curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+    cp /etc/apt/sources.list /etc/apt/sources.list.bak
+    echo "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" >> /etc/apt/sources.list
+    apt-get update && apt-get install -y docker-ce=$(apt-cache madison docker-ce | grep 18.06 | head -1 | awk '{print $3}')  docker-compose
+echo "========================================="
+echo "|| Docker and docker-compose installed ||"
+echo "========================================="
 
-apt-get update 
-apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common  && \
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add - && 
-add-apt-repository \
-   "deb https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
-   $(lsb_release -cs) \
-   stable"
-apt-get update && apt-get install -y docker-ce=$(apt-cache madison docker-ce | grep 17.03 | head -1 | awk '{print $3}')
+echo ""
+echo ""
+echo ""
+echo "Would you like test? "
+echo "Continue? type y or n"
+read contStartCompose
+case "$contStartCompose" in
+    y|Y) echo "Ok. Its test docker"
+    docker run hello-world
+    echo "========================================="
+    echo "|| Test GOOD ? Yes or no? Type 'y' or 'n' ||"
+    echo "========================================="
+    read testGoodOrNot
+         ;;
+    n|N) echo "Ok it's your choice. Bay Bay"
+        exit 0
+        ;;
+    *) echo "Sorry need type"
+        ;;
+esac
+rm -rf /var/lib/docker
