c9474a98e965cb258a30be7ca19d415ae77121e5
Oleg Chulanov <oleg.chulanov@gmail.com>
2019-01-22 19:54:12 +0200

    run

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
-docker run -d -p 9000 --name portainer --restart always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
+mkdir /config/
+docker run -d -p 8080:8080 --name portainer --restart always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
 docker build -t nginx-kito ./docker/nginx/
 docker build -t hello-cont ./docker/app/
 
-docker run -v /var/www:/var/www -p 9000 -t hello-cont
+docker run --name nginx-lb --restart always-d -p 80:80 443:443 -t nginx-kito -v /config:/etc/nginx
+docker run --name hello1 -v /var/www:/var/www -p 8080:8080 -t hello-cont
+docker run --name hello2 -v /var/www:/var/www -p 8080:8080 -t hello-cont
