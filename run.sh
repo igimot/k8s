c40198508ca104ef3e85cecfeebf812031470e48
Oleg Chulanov <oleg.chulanov@gmail.com>
2019-01-22 20:14:50 +0200

    run 5

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 mkdir /config/
-docker run -d -p 8080:8080 --name portainer --restart always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
-docker build -t nginx-kito ./docker/nginx/
+docker run -d -p 80:80 --restart always -v /var/run/docker.sock:/tmp/docker.sock:ro jwilder/nginx-proxy
+docker run -e VIRTUAL_HOST=hello-portiner.com --expose=80 d -p 8080:8080 --name portainer --restart always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
+
+#docker build -t nginx-kito ./docker/nginx/
 docker build -t hello-cont ./docker/app/
 
-docker run --name nginx-lb --restart always-d -p 80:80 443:443 -t nginx-kito -v /config:/etc/nginx
-docker run --name hello1 -v /var/www:/var/www -p 8080:8080 -t hello-cont
-docker run --name hello2 -v /var/www:/var/www -p 8080:8080 -t hello-cont
+#docker run -d -p 80:80 -v /var/run/docker.sock:/tmp/docker.sock:ro jwilder/nginx-proxy
+docker run --name hello1 VIRTUAL_HOST=hello1.com --restart always --expose=80 -v /var/www:/var/www  -t hello-cont
+docker run --name hello2 VIRTUAL_HOST=hello1.com --restart always --expose=80 -v /var/www:/var/www  -t hello-cont
